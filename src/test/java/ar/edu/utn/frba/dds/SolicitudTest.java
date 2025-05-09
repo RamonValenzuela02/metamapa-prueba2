21874bab3cbd6a421b1bfa5780c0d3e2ade6c926
Lorens330 <lsavva@frba.utn.edu.ar>
2025-05-09 00:16:42 -0300

    Arreglo SolicitudTest

--- src/test/java/ar/edu/utn/frba/dds/SolicitudTest.java
+++ src/test/java/ar/edu/utn/frba/dds/SolicitudTest.java
@@ -1,46 +1,61 @@
 package ar.edu.utn.frba.dds;
 
 import org.junit.jupiter.api.Test;
-
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 public class SolicitudTest {
-  List<SolicitudDeEliminacion> solicitudes = new ArrayList<>();
-  FuenteCSV fuenteCSV = new FuenteCSV("C:\\Users\\santi\\OneDrive\\Documentos\\UTN\\Diseño\\Java\\tpa-2025-26\\src\\test\\java\\ar\\edu\\utn\\frba\\dds\\prueba.csv");
-  List<Hecho> hechos = fuenteCSV.getHechos();
-  String titulo = hechos.get(0).getTitulo();
-  String motivo = "Ubicacion erronea";
-  SolicitudDeEliminacion solicitud = new SolicitudDeEliminacion(titulo, motivo, solicitudes, fuenteCSV); // req 5
+
   @Test
-  public void solicitud(){
+  public void solicitud() { // req 5
+    List<SolicitudDeEliminacion> solicitudes = new ArrayList<>();
+    FuenteCSV fuenteCSV = new FuenteCSV("D:\\UTN pc\\3er año\\Diseño de Sistemas de Informacion\\tpa-2025-26\\src\\test\\java\\ar\\edu\\utn\\frba\\dds\\prueba.csv");
+    String titulo = fuenteCSV.getHechos().get(0).getTitulo();
+    String motivo = "Ubicacion erronea";
+
+    SolicitudDeEliminacion solicitud = new SolicitudDeEliminacion(titulo, motivo, solicitudes, fuenteCSV);
+
     assertEquals(1, solicitudes.size());
   }
+
   @Test
-  public void aceptarSolicitud(){ // req 6
-    List<SolicitudDeEliminacion> pendientes = solicitudes.stream().
-            filter(soli -> soli.getEstado().
-            equals(Estado.PENDIENTE)).collect(Collectors.toList());
+  public void aceptarSolicitud() { // req 6
+    List<SolicitudDeEliminacion> solicitudes = new ArrayList<>();
+    FuenteCSV fuenteCSV = new FuenteCSV("D:\\UTN pc\\3er año\\Diseño de Sistemas de Informacion\\tpa-2025-26\\src\\test\\java\\ar\\edu\\utn\\frba\\dds\\prueba.csv");
+    String titulo = fuenteCSV.getHechos().get(0).getTitulo();
+    String motivo = "Ubicacion erronea";
+
+    new SolicitudDeEliminacion(titulo, motivo, solicitudes, fuenteCSV);
+
+    List<SolicitudDeEliminacion> pendientes = solicitudes.stream()
+        .filter(soli -> soli.getEstado().equals(Estado.PENDIENTE))
+        .collect(Collectors.toList());
+
     SolicitudDeEliminacion soliAanalizar = pendientes.get(0);
     soliAanalizar.aceptar();
-    pendientes.remove(0);
-    assertEquals(0, pendientes.size());
+
     assertEquals(Estado.ACEPTADA, soliAanalizar.getEstado());
   }
+
   @Test
-  public void rechazarSolicitud(){ // req 6
-    List<SolicitudDeEliminacion> pendientes = solicitudes.stream().
-        filter(soli -> soli.getEstado().
-            equals(Estado.PENDIENTE)).collect(Collectors.toList());
+  public void rechazarSolicitud() {  // req 6
+    List<SolicitudDeEliminacion> solicitudes = new ArrayList<>();
+    FuenteCSV fuenteCSV = new FuenteCSV("D:\\UTN pc\\3er año\\Diseño de Sistemas de Informacion\\tpa-2025-26\\src\\test\\java\\ar\\edu\\utn\\frba\\dds\\prueba.csv");
+    String titulo = fuenteCSV.getHechos().get(0).getTitulo();
+    String motivo = "Ubicacion erronea";
+
+    new SolicitudDeEliminacion(titulo, motivo, solicitudes, fuenteCSV);
+
+    List<SolicitudDeEliminacion> pendientes = solicitudes.stream()
+        .filter(soli -> soli.getEstado().equals(Estado.PENDIENTE))
+        .collect(Collectors.toList());
+
     SolicitudDeEliminacion soliAanalizar = pendientes.get(0);
     soliAanalizar.rechazar();
-    pendientes.remove(0);
-    assertEquals(0, pendientes.size());
+
     assertEquals(Estado.RECHAZADA, soliAanalizar.getEstado());
   }
 }
