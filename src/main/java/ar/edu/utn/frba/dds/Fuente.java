b0d4d82bcdf5c1ef8aefb7397a9fdfd61901f55f
Ramon Valenzuela <rvalenzuela@frba.utn.edu.ar>
2025-06-05 13:49:43 -0300

    agrego obtenerHechosCumplidoresConCriterios en Funte

--- src/main/java/ar/edu/utn/frba/dds/Fuente.java
+++ src/main/java/ar/edu/utn/frba/dds/Fuente.java
@@ -1,7 +1,16 @@
 package ar.edu.utn.frba.dds;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface Fuente {
-  public List<Hecho> obtenerHechosConCriterio(Criterio criterio);
+  public  List<Hecho> obtenerHechosConCriterio(Criterio criterio);
+
+  public default List<Hecho> obtenerHechosConCriterios(List<Criterio> criterios) {
+    List<Hecho> hechosCumplidores = new ArrayList<>();
+    for(Criterio criterio : criterios) {
+      hechosCumplidores.addAll(obtenerHechosConCriterio(criterio));
+    }
+    return hechosCumplidores;
+  }
 }
