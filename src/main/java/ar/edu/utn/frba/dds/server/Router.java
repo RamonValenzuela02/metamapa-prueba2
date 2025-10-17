35a4c3a923b1ea18df6cf57723bd3d7599e9b5f0
Ramon Valenzuela <rvalenzuela@frba.utn.edu.ar>
2025-10-17 20:13:56 -0300

    el server arranca

--- src/main/java/ar/edu/utn/frba/dds/server/Router.java
+++ src/main/java/ar/edu/utn/frba/dds/server/Router.java
@@ -3,14 +3,17 @@ package ar.edu.utn.frba.dds.server;
 import ar.edu.utn.frba.dds.controllers.HomeController;
 import io.github.flbulgarelli.jpa.extras.test.SimplePersistenceTest;
 import io.javalin.Javalin;
+import java.util.HashMap;
 
 public class Router implements SimplePersistenceTest {
   public void configure(Javalin app) {
     HomeController controller = new HomeController();
 
+    /*
     app.before(ctx -> {
       entityManager().clear();
     });
+     */
     app.get("/", context -> context.redirect("/home"));
     app.get("/home", ctx -> ctx.render("home.hbs", controller.index(ctx)));
   }
