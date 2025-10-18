f129b022f44cee86e3cd1e0979032b83c40719b2
Ramon Valenzuela <rvalenzuela@frba.utn.edu.ar>
2025-10-18 08:50:36 -0300

    avanzo con requerimientos 1 y 2

--- src/main/java/ar/edu/utn/frba/dds/controllers/HomeController.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/HomeController.java
@@ -1,16 +1,21 @@
 package ar.edu.utn.frba.dds.controllers;
 
 import ar.edu.utn.frba.dds.model.Hecho;
+import ar.edu.utn.frba.dds.model.criterio.Categoria;
 import ar.edu.utn.frba.dds.model.fuente.Fuente;
+import ar.edu.utn.frba.dds.model.ubicacion.Ubicacion;
 import ar.edu.utn.frba.dds.repositorios.RepoFuentesDelSistema;
+import ar.edu.utn.frba.dds.repositorios.RepoHechosDinamicos;
 import io.javalin.http.Context;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import org.jetbrains.annotations.NotNull;
 
-public class HomeController {
+public class HomeController{
   public Map<String,Object> index(@NotNull Context ctx) {
     List<Fuente> fuentes = RepoFuentesDelSistema.getInstance().obtenerFuentes();
     List<Hecho> hechos = fuentes.stream()
@@ -22,4 +27,36 @@ public class HomeController {
 
     return model;
   }
+
+  public void crearHecho(@NotNull Context ctx) {
+    String titulo = ctx.formParam("titulo");
+    String descripcion = ctx.formParam("descripcion");
+    Categoria categoria = Categoria.valueOf(ctx.formParam("categoria"));
+    String latitud = ctx.formParam("latitud");
+    String longitud = ctx.formParam("longitud");
+    LocalDateTime fechaHecho = LocalDateTime.parse(Objects.requireNonNull(ctx.formParam("fechaHecho")));
+
+    Hecho hecho = new Hecho(
+      titulo,
+      descripcion,
+      categoria,
+      latitud,
+      longitud,
+      fechaHecho,
+      LocalDateTime.now()
+    );
+    //RepoHechosDinamicos repo = RepoHechosDinamicos.getInstance();
+    //repo.withTransaction(() -> repo.agregarHecho(hecho));
+
+    ctx.redirect("/home");
+  }
+
+  /*
+  public Map<String,Object> solicitarEliminacionForm(@NotNull Context ctx) {
+  }
+
+  public void solicitarEliminacion(@NotNull Context context) {
+  }
+    */
+
 }
--- src/main/java/ar/edu/utn/frba/dds/server/Router.java
+++ src/main/java/ar/edu/utn/frba/dds/server/Router.java
@@ -14,7 +14,29 @@ public class Router implements SimplePersistenceTest {
       entityManager().clear();
     });
      */
+
+
+    //REQUERIMIENTO 1 (me falta lo del mapa)
     app.get("/", context -> context.redirect("/home"));
-    app.get("/home", ctx -> ctx.render("home.hbs", controller.index(ctx)));
+    app.get("/home", ctx -> ctx.render("home.hbs", new HashMap<>()));
+
+    //REQUERIMIENTO 2
+    //crear hecho
+    app.get("/hecho/nuevo", ctx -> ctx.render("hecho.nuevo.hbs", new HashMap<>()));
+    app.post("/hechos", controller::crearHecho);
+    //crear solicitud de eliminacion
+    //app.get("/hechos/:id/eliminar", ctx -> ctx.render("hechos.eliminar.hbs", controller.solicitarEliminacionForm(ctx)));
+    //app.post("/hechos/:id/eliminar", controller::solicitarEliminacion);
+
+    //REQUERIMIENTO 3
+    //login
+    app.get("/login", ctx -> ctx.result("login"));
+
+    //REQUERIMIENTO 4
+    //REQUERIMIENTO 5
+    //REQUERIMIENTO 6
+    //REQUERIMIENTO 7
+    //REQUERIMIENTO 8
+
   }
 }
