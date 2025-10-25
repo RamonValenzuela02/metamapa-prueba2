592d6bd50b9ea109fce756b19a4e693a4d158e6e
frannnmv <fmorenovera@frba.utn.edu.ar>
2025-10-24 22:43:21 -0300

    agrego logica para ocultar barra navegacion

--- src/main/java/ar/edu/utn/frba/dds/controllers/SessionController.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/SessionController.java
@@ -19,6 +19,8 @@ public class SessionController implements WithSimplePersistenceUnit, Transaction
     }
 
     Map<String, Object> modelo = new HashMap<>();
+    modelo.put("esconderNav", true);
+
     if ("true".equals(ctx.queryParam("error"))) {
       modelo.put("error", "Usuario o contraseña inválidas");
     }
